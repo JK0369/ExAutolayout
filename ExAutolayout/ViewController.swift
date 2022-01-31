9ac5bcb5583892b145fbf21747dd9e6b5f6eebd6
jake <palatable7@naver.com>
2022-02-01 01:29:43 +0900

    no message

--- ExAutolayout/ViewController.swift
+++ ExAutolayout/ViewController.swift
@@ -9,35 +9,48 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-  private let leftLabel: UILabel = {
-    let label = UILabel()
-    label.text = "iOS 앱 개발 알아가기 jake 블로그 길고 긴 UILabel의 텍스트"
-    label.textColor = .white
-    label.backgroundColor = .blue
-    return label
+  private let progressContainerView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .gray
+    return view
   }()
-  private let rightLabel: UILabel = {
-    let label = UILabel()
-    label.text = "label 2 알아가기 jake 블로그 길고 긴 UILabel의 텍스트"
-    label.textColor = .white
-    label.backgroundColor = .orange
-    return label
+  private let progressView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .systemBlue
+    return view
   }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    self.view.addSubview(self.leftLabel)
-    self.view.addSubview(self.rightLabel)
+    self.title = "remake, multipliedBy 예제 (progress UI)"
+    self.view.addSubview(self.progressContainerView)
+    self.progressContainerView.addSubview(self.progressView)
     
-    self.leftLabel.snp.makeConstraints {
-      $0.top.equalToSuperview().inset(56)
-      $0.left.equalToSuperview()
+    self.progressContainerView.snp.makeConstraints {
+      $0.top.equalTo(self.view.safeAreaLayoutGuide)
+      $0.left.right.equalToSuperview().inset(32)
+      $0.height.equalTo(20)
     }
-    self.rightLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
-    self.rightLabel.snp.makeConstraints {
-      $0.top.equalToSuperview().inset(56)
-      $0.left.equalTo(self.leftLabel.snp.right)
-      $0.right.equalToSuperview()
+    self.progressView.snp.makeConstraints {
+      $0.left.bottom.top.equalToSuperview()
+      $0.width.equalToSuperview().multipliedBy(progress / finish)
     }
+    
+    updateProgress { [weak self] in
+      self?.progressView.snp.remakeConstraints {
+        $0.left.bottom.top.equalToSuperview()
+        $0.width.equalToSuperview().multipliedBy(progress / finish)
+      }
+    }
+  }
+}
+
+var progress = 1.0
+var finish = 100.0
+func updateProgress(completion: @escaping () -> Void) {
+  Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+    progress += 1
+    completion()
   }
 }
