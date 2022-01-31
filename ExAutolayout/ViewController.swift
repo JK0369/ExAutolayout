0c7b37d220af024c76aa89b266e8994094c41234
jake <palatable7@naver.com>
2022-02-01 00:58:49 +0900

    no message

--- ExAutolayout/ViewController.swift
+++ ExAutolayout/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+  private let leftLabel: UILabel = {
+    let label = UILabel()
+    label.text = "iOS 앱 개발 알아가기 jake 블로그 길고 긴 UILabel의 텍스트"
+    label.textColor = .white
+    label.backgroundColor = .blue
+    return label
+  }()
+  private let rightLabel: UILabel = {
+    let label = UILabel()
+    label.text = "label 2 알아가기 jake 블로그 길고 긴 UILabel의 텍스트"
+    label.textColor = .white
+    label.backgroundColor = .orange
+    return label
+  }()
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    self.view.addSubview(self.leftLabel)
+    self.view.addSubview(self.rightLabel)
+    
+    self.leftLabel.snp.makeConstraints {
+      $0.top.equalToSuperview().inset(56)
+      $0.left.equalToSuperview()
+    }
+    self.rightLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+    self.rightLabel.snp.makeConstraints {
+      $0.top.equalToSuperview().inset(56)
+      $0.left.equalTo(self.leftLabel.snp.right)
+      $0.right.equalToSuperview()
+    }
   }
-
-
 }
-
