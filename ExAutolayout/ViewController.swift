11dd7f82228a3f1198a81c72ca6a6214ca3b4846
jake <palatable7@naver.com>
2022-02-01 23:52:46 +0900

    Fix some code

--- ExAutolayout/ViewController.swift
+++ ExAutolayout/ViewController.swift
@@ -17,12 +17,12 @@ class ViewController: UIViewController {
   }()
   private let label: UILabel = {
     let view = UILabel()
-    view.font = .systemFont(ofSize: 32)
-    view.textColor = .gray
+    view.font = .systemFont(ofSize: 24)
+    view.textColor = .white
     view.numberOfLines = 0
-    view.backgroundColor = .systemBlue.withAlphaComponent(0.2)
+    view.backgroundColor = .systemBlue
     let text = "iOS 앱 개발 알아가기 jake 블로그 autolayout 고급 \n iOS 앱 개발 알아가기 jake 블로그 autolayout 고급\n iOS 앱 개발 알아가기 jake 블로그 autolayout 고급 \n"
-    view.text = text + text + text
+    view.text = text + text + text + text
     return view
   }()
   private let imageView: UIImageView = {
@@ -36,16 +36,12 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     
     let imageViewContainer = UIView()
+    self.view.backgroundColor = self.label.backgroundColor
     self.view.addSubview(self.scrollView)
     self.scrollView.addSubview(imageViewContainer)
     self.scrollView.addSubview(self.imageView)
     self.scrollView.addSubview(self.label)
     
-    // scrollView 구현 핵심
-    // 내부 뷰들의 layout에 주의
-    // 1. left, right는 self.view를 가지게 할 것: equalToSuperview() 하게되면 수평 스크롤 되는 현상 발생
-    // 2. 내부 뷰들의 가장 위, 아래에 있는 view는 equalToSuperview()를 따로도록 설정
-    // 3. 내부 뷰들의 크기는 내부 크기에 따라 자동으로 늘어나는 UILabel intrinsicContentSize가 있어야함 (UITextView를 그냥 사용할시 적용 x 주의)
     self.scrollView.snp.makeConstraints {
       $0.edges.equalToSuperview()
     }
@@ -60,8 +56,8 @@ class ViewController: UIViewController {
       $0.height.greaterThanOrEqualTo(imageViewContainer.snp.height) // <-
     }
     self.label.snp.makeConstraints {
-      $0.left.right.equalTo(self.view)
       $0.top.equalTo(imageViewContainer.snp.bottom)
+      $0.left.right.equalTo(self.view)
       $0.bottom.equalToSuperview()
     }
     
